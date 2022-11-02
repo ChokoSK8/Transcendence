05cf06490ef24a36e7d5a045afdede43302458ec
Antoine BRUN <abrun@e1r11p5.clusters.42paris.fr>
2022-11-02 10:53:37 +0100

    fix INestApplication error

--- srcs/volumes/NestFirst/test/app.e2e-spec.ts
+++ srcs/volumes/NestFirst/test/app.e2e-spec.ts
@@ -1,14 +1,20 @@
-import { Test } from '@nestjs/testing';
+import { Test, INestApplication } from '@nestjs/testing';
 import { nestFirstModule } from '../src/nestFirst.module';
+import { ValidationPipe } from '@nestjs/common';
 
 describe('App e2e', () => {
+	let	app: INestApplication;
+
 	beforeAll(async () => {
+		// easiest way to instanciate a module, here a nestFirstModule
 		const moduleRef = 
 			await Test.createTestingModule({
 				imports: [nestFirstModule],
 			}).compile();
 
-		const app = moduleRef.createNestApplication();
+		// create a app that emulates our application
+		app = moduleRef.createNestApplication();
+		app.useGlobalPipes(new ValidationPipe({whitelist: true}));
 	});
 	it.todo('should pass');
 });
---
+++ srcs/volumes/NestFirst/typescript_tuto/enum.ts
@@ -0,0 +1,8 @@
+	// enum is usefull to distinct from other values not what the values they're pointing
+	// values are auto increment
+export enum DIRECTIONS {
+	UP,
+	DOWN,
+	RIGHT,
+	LEFT
+}
